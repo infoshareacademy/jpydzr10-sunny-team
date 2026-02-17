2600628581781f5aa7ef5798bc82e6736739fa57
Grzegorz Tomza <firroth1991@gmail.com>
2026-02-17 15:32:46 +0100

    JPY101-11 add basic login data validation

--- auth/login.py
+++ auth/login.py
@@ -5,9 +5,22 @@ def hash_password(password: str):
     return hashlib.sha256(password.encode()).hexdigest()
 
 def login(username: str, password: str):
+    # Walidacja danych logowania. (Chcemy walidacje dopiero po znalezieniu użytkownika)
+    validate_login_data(username, password)
+
     for user in user_database.values():
         if user.username == username and user.password_hash == hash_password(password):
             return user
 
     return None
 
+# Walidacja danych logowania
+def validate_login_data(username, password):
+    if not username:
+        raise ValueError('Username is required')
+
+    if not password:
+        raise ValueError('Password is required')
+
+    if len(password) < 6:
+        raise ValueError('Password must be at least 6 characters long')
