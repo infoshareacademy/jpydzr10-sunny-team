307f473f8a85a23d042f1d6f4bd526853bb764c9
Patrykbartczak <patryk.bartczak99@gmail.com>
2026-02-17 18:58:23 +0100

    Correct user_database in database.py, admin_check.py

--- database/database.py
+++ database/database.py
@@ -1,7 +1,7 @@
 from models.user_employee import User
 from models.role import Admin
 
-user_databse = {}
+user_database = {}
 
 def create_user(user_id: int, username: str ,password_hash: str , role: str):
     if role == "Admin":
@@ -10,6 +10,6 @@ def create_user(user_id: int, username: str ,password_hash: str , role: str):
         user = User(user_id, username, password_hash)
     key = user.user_id
 
-    user_databse[key] = user
+    user_database[key] = user
 
     return user
