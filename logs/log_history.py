1e832677830c382b3ebf2c2d7e8b7cb4579e2c42
Patrykbartczak <patryk.bartczak99@gmail.com>
2026-02-21 14:59:21 +0100

    Update definition of class, and filename

--- logs/log_history.py
+++ logs/log_history.py
@@ -1,7 +1,13 @@
 from datetime import datetime
 
 class ChangeLog:
-    """Model zapisywania zmian"""
+    """Model zapisywania zmian
+    user_id - numer uzytkownika w bazie danych
+    action - jaka akcja jest wykonywana usun/dodaj/itp.
+    object_type - na kim jest ta akcja user/admin/manger/itd.
+    date - kiedy zostalo to zrobione
+
+    """
     def __init__(self,user_id,action,object_type,):
         self.user_id = user_id
         self.action = action
@@ -13,5 +19,10 @@ class LogHistory:
         self.log_history = []
 
     def add_new_change(self, user_id, action, object_type):
+        """Metoda nowych zmian
+    user_id - numer uzytkownika w bazie danych
+    action - jaka akcja jest wykonywana usun/dodaj/itp.
+    object_type - na kim jest ta akcja user/admin/manger/itd.
+    """
         new_log = ChangeLog(user_id,action,object_type)
         self.log_history.append(new_log)
