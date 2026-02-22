6f170e1026998d722eefd33ca53312baa9f16f05
Kubek0212 <kubek0212@gmail.com>
2026-02-22 20:18:25 +0100

    moved permission dictionday inside a class

--- Permission System/permission.py
+++ Permission System/permission.py
@@ -1,1 +1,24 @@
-class 
+class Permission:
+    def __init__(self):
+        self.permissions = {    "Admin":    {"can_1": False,
+                                            "can_2": False,
+                                            "can_3": False,
+                                            "can_4": False},
+                                "hr":       {"can_1": False,
+                                            "can_2": False,
+                                            "can_3": False,
+                                            "can_4": False},
+                                "manager":  {"can_1": False,
+                                            "can_2": False,
+                                            "can_3": False,
+                                            "can_4": False},
+                                "user":     {"can_1": False,
+                                            "can_2": False,
+                                            "can_3": False,
+                                            "can_4": False},
+                                "worker":   {"can_1": False,
+                                            "can_2": False,
+                                            "can_3": False,
+                                            "can_4": False}}
+
+    def verifyPermission(self, user, command):
