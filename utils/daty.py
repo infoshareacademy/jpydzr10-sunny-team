f16a46a39bb4587bf67428cd1c857d787bf2fcc8
Grzegorz Tomza <firroth1991@gmail.com>
2026-03-06 17:59:53 +0100

    Remove unused function and change name of attributes

--- utils/daty.py
+++ utils/daty.py
@@ -1,20 +1,26 @@
-# Tu trzymamy wszystko co związane z datami, urlopami, świętami
+"""Tu trzymamy wszystko co związane z datami, urlopami, świętami"""
 
 import calendar
-from datetime import date, datetime, timedelta
-import tempora
+from datetime import date
 
-def is_working_day(dzien: date):
-    if calendar.weekday(dzien.year, dzien.month, dzien.day) >= 5:
+"""Odkomentowac jesli biblioteki tempora potrzebne"""
+#import tempora
+
+def is_working_day(d: date):
+    if d.weekday() >= 5: #Usuwamy sobote i niedziele z listy dnich roboczych
         return False
     return True
 
-def is_weekend(dzien: date) :
-    return dzien.weekday() >= 5
+def is_weekend(d: date):
+    return d.weekday() >= 5
 
 def date_format(d: date):
     return d.strftime("%d.%m.%Y (%a)")
 
-def current_in_utc():
-    """zwraca aktualny czas w UTC"""
-    return datetime.now
+def working_day_list(self, month):
+    working_days = []
+    for day in range(1, calendar.monthrange(self.year, month)[1] + 1):
+        data = date(self.year, month, day)
+        if self.is_working_day(data):
+            working_days.append(data)
+    return working_days
