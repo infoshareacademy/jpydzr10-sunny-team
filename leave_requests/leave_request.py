e455c0e58c8c89739e8fafbb226385c374cb4897
Kubek0212 <kubek0212@gmail.com>
2026-03-08 02:10:17 +0100

    removed who_confirmed from LeaveRequest init

--- leave_requests/leave_request.py
+++ leave_requests/leave_request.py
@@ -11,10 +11,15 @@ class LeaveRequest:
         self.status_leave = "pending" #pending - oczegujący na decyzję, approved/declined - decyzja
         if amount_days > 0: #TODO: wstawić pointer do pracownika i dostępnego czasu
             self.status_leave = "declined"
-        self.who_confirmed = who_confirmed
+        # self.who_confirmed = who_confirmed
+        # self.who_declined = who_declined
         # self.status = status
 
-    def confirmed_leave(self,who_confirmed ):
+    def confirmed_leave(self,who_confirmed):
         self.status_leave = "approved"
         self.who_confirmed = who_confirmed
+
+    def declined_leave(self,who_declined):
+        self.status_leave = "declined"
+        self.who_declined = who_declined
 
