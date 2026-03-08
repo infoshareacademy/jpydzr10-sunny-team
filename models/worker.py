eac63c53b8e2faf7052f421f0eb677f3243727ea
Kubek0212 <kubek0212@gmail.com>
2026-03-08 11:28:13 +0100

    added leave request list property in worker class

--- models/worker.py
+++ models/worker.py
@@ -1,6 +1,8 @@
 from datetime import date
 from user import User
 
+from leave_requests.leave_request import LeaveRequest
+
 class Worker(User):
     def __init__(self, user_id: int, username: str, password_hash: str, first_name: str, last_name: str, hire_date: date, total_leave_days: int, used_leave_days: int, role = "Worker"):
         super().__init__(user_id, username, password_hash, role)
@@ -9,3 +11,7 @@ class Worker(User):
         self.hire_date = hire_date
         self.total_leave_days = total_leave_days
         self.used_leave_days = used_leave_days
+        self.leave_requests_list = []
+
+    def add_leave_request(self, leave_request: LeaveRequest):
+        self.leave_requests_list.append(leave_request)
