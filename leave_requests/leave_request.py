8da5532f6185f2dd42b421ac267a17c452449b76
Kubek0212 <kubek0212@gmail.com>
2026-03-08 02:19:31 +0100

    comparing requested leave days to user data in database

--- leave_requests/leave_request.py
+++ leave_requests/leave_request.py
@@ -1,6 +1,7 @@
+from database.database import load_users
 
 class LeaveRequest:
-    def __init__(self,employee_id:int, first_name:str, last_name:str, start_date:str, end_date:str, amount_days:int, who_confirmed):
+    def __init__(self,employee_id:int, first_name:str, last_name:str, start_date:str, end_date:str, amount_days:int):
         """Klasa opisująca wniosek urlopowy pracownika"""
         self.employee_id = employee_id
         self.first_name = first_name
@@ -9,7 +10,8 @@ class LeaveRequest:
         self.end_date = end_date
         self.amount_days = amount_days
         self.status_leave = "pending" #pending - oczegujący na decyzję, approved/declined - decyzja
-        if amount_days > 0: #TODO: wstawić pointer do pracownika i dostępnego czasu
+        user_list = load_users()
+        if amount_days < user_list[employee_id].total_leave_days - user_list[employee_id].used_leave_days:
             self.status_leave = "declined"
         # self.who_confirmed = who_confirmed
         # self.who_declined = who_declined
