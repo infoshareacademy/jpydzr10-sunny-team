ac65589864c41eb4771c2141207dd96d3f89aa62
Patrykbartczak <patryk.bartczak99@gmail.com>
2026-03-25 17:31:36 +0100

    Update for admin_exist in admin_check.py

--- startup/admin_check.py
+++ startup/admin_check.py
@@ -6,4 +6,4 @@ from database.database import user_database,create_user
 def admin_exist():
     if not (any(isinstance(user,Admin) for user in user_database.values())):
         print("Brak admina w bazie danych. Tworzenie SuperAdmina.")
-        create_user(1,"SuperAdmin","1234","Admin")
+        create_user(1,"SuperAdmin","123456","Admin")
