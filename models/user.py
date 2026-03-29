0ce0fb7998f0ef061a2643ee4dfca77e361a574d
wiola szymecka <wioleta.szymecka@gmail.com>
2026-03-29 21:13:40 +0200

    Add new parameter is_active to models/user.py

--- models/user.py
+++ models/user.py
@@ -3,8 +3,12 @@ class User:
     Reprezentuje konto użytkownika w systemie urlopowym.
     Zawiera dane potrzebne do logowania i kontroli dostępu.
     """
-    def __init__(self, user_id: int, username: str, password_hash: str, role: str):
+    def __init__(self, user_id: int, username: str, password_hash: str, role: str, is_active: bool = True):
         self.user_id = user_id
         self.username = username
         self.password_hash = password_hash
         self.role = role
+        self.is_active = is_active
+
+    def deactivate(self):
+        self.is_active = False
