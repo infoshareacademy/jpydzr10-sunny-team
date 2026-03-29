40a3a22aed428edbc7830f4474aed339656d6745
wiola szymecka <wioleta.szymecka@gmail.com>
2026-03-29 21:19:07 +0200

    Add new parameter is_active to models/manager.py

--- models/manager.py
+++ models/manager.py
@@ -2,5 +2,5 @@ from models.worker import Worker
 from datetime import date
 
 class Manager(Worker):
-    def __init__(self, user_id: int, username: str, password_hash: str,first_name: str, last_name: str, hire_date: date, total_leave_days: int, used_leave_days: int, role = "Manager"):
-        super().__init__(user_id, username, password_hash, first_name, last_name, hire_date, total_leave_days, used_leave_days, role)
+    def __init__(self, user_id: int, username: str, password_hash: str,first_name: str, last_name: str, hire_date: date, total_leave_days: int, used_leave_days: int, role = "Manager", is_active: bool = True):
+        super().__init__(user_id, username, password_hash, first_name, last_name, hire_date, total_leave_days, used_leave_days, role, is_active)
