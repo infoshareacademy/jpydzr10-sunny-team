754396299a5251dfee15b69507c3b45777bf7684
Patrykbartczak <patryk.bartczak99@gmail.com>
2026-04-20 19:00:41 +0200

    JPY101-42 setup Django project + 4 apps

--- startup/startup_app.py
+++ startup/startup_app.py
@@ -10,7 +10,7 @@ from database.database import load_users, create_user, user_database, save_users
 from database.leave_requests_db import load_leave_requests, save_leave_requests
 from leave_requests.leave_request import LeaveRequest
 from leave_requests.display_vacations import display_vacations
-from logs.log_history import app_log
+from logs_old.log_history import app_log
 from permission_system.permission import Permission
 
 
