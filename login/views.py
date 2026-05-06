c7cdaa76431e7748731ee0a7b03d7332efddf8bf
Kubek0212 <kubek0212@gmail.com>
2026-05-06 22:53:18 +0200

    added login_required decorator

--- login/views.py
+++ login/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 def login_view(request):
     if request.method == 'POST':
