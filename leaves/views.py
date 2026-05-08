f74ce0267d65c9c43c58b22ca99e644bbf47fcad
Grzegorz Tomza <firroth1991@gmail.com>
2026-05-08 17:54:18 +0200

    Add logic for only the logged user to see his own vacations

--- leaves/views.py
+++ leaves/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from datetime import date
+from leave_requests.display_vacations import vacations
 
 
 @login_required(login_url='/accounts/login/')
@@ -17,54 +18,31 @@ def all_requests_list(request):
 @login_required
 def my_vacations(request):
 
+    user = request.user
     today = date.today()
+
+    my_vacation = [v for v in vacations if v['employee_id'] == user.id]
 
     # TODO: Tutaj trzeba załadować wnioski danego użytkownika
     # Na razie wstawiam mock data - później zmienimy na prawdziwe ładowanie z DB
-
-    mock_vacations = [
-        {
-            'id': 1,
-            'start_date': date(2026, 5, 5),
-            'end_date': date(2026, 5, 15),
-            'amount_days': 11,
-            'type': 'Wypoczynkowy',
-            'status': 'W trakcie',
-        },
-            {
-            'id': 2,
-            'start_date': date(2026, 6, 10),
-            'end_date': date(2026, 6, 20),
-            'amount_days': 11,
-            'type': 'Wypoczynkowy',
-            'status': 'Zatwierdzony',
-        },
-        {
-            'id': 3,
-            'start_date': date(2026, 3, 1),
-            'end_date': date(2026, 3, 10),
-            'amount_days': 10,
-            'type': 'Wypoczynkowy',
-            'status': 'Zatwierdzony',
-        },
-    ]
 
     current = []
     planned = []
     archival = []
 
-    for req in mock_vacations:
-        if req['start_date'] <= today <= req['end_date']:
-            current.append(req)
-        elif req['start_date'] > today:
-            planned.append(req)
+    for vacation in my_vacation:
+        if vacation['start_date'] <= today <= vacation['end_date']:
+            current.append(vacation)
+        elif vacation['start_date'] > today:
+            planned.append(vacation)
         else:
-            archival.append(req)
+            archival.append(vacation)
 
     context = {
         'current_vacations': current,
         'planned_vacations': planned,
         'archival_vacations': archival,
+        'user': user,
     }
 
     return render(request, 'leaves/my_vacations.html', context)
