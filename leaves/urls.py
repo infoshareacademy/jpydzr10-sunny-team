b68e96feb1be93b371c09fec08dab4faeb66dd04
nowak-kamil <nkamil2002@gmail.com>
2026-05-08 17:35:30 +0200

    Done new_request, IMPORTANT TO DO: implement leave request submission and available days export/update

--- leaves/urls.py
+++ leaves/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
     path('my-vacations/', views.vacation_list, name='vacation_list'),
     path('all-requests/', views.all_requests_list, name='all_requests_list'),
+    path('new-request/', views.new_request, name='new_request'),
+    path('calculate-days/', views.calculate_days_api, name='calculate_days_api'),
+
 ]
