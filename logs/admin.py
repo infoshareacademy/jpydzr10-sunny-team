ab7bb8f94617836e1d46696725e5446ccd322872
Patrykbartczak <patryk.bartczak99@gmail.com>
2026-05-12 20:41:04 +0200

    JPY101-77 ChangeLogAdmin tylko do odczytu: has_add_permission=False, has_change_permission=False

--- logs/admin.py
+++ logs/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from logs.models import ChangeLog
 
-# Register your models here.
+
+@admin.register(ChangeLog)
+class ChangeLogAdmin(admin.ModelAdmin):
+    readonly_fields = ["who","action","object_type","created_at"]
+    def has_add_permission(self,request):
+        return False
+
+    def has_change_permission(self,request,obj = None):
+        return False
