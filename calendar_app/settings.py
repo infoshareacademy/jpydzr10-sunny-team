cf0f0fc6351f942384f5064109ee0b2a33982648
Patrykbartczak <patryk.bartczak99@gmail.com>
2026-05-12 19:07:43 +0200

    JPY101-58 makemigrations + migrate + konfiguracja MySQL

--- calendar_app/settings.py
+++ calendar_app/settings.py
@@ -84,12 +84,17 @@ WSGI_APPLICATION = 'calendar_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'calendar_db',
+        'USER': 'root',
+        'PASSWORD': '12345678',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'unix_socket': '/tmp/mysql.sock',
+        },
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
