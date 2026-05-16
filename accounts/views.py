443c5428c87f4e7905c97f9da65b3e9b9a8419eb
Kubek0212 <kubek0212@gmail.com>
2026-05-16 23:56:09 +0200

    added view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import User
+from .permission import Permission
 
-# Create your views here.
+
+@login_required
+def deactivate_user(request, pk):
+    target_user = get_object_or_404(User, pk=pk)
+
+    if target_user.role in ('HR', 'Manager'):
+        required_action = 'can_deactivate_staff'
+    else:
+        required_action = 'can_deactivate_worker'
+
+    if not Permission.verifyPermission(request.user.role, required_action):
+        return redirect('dashboard')
+
+    if request.method == 'POST':
+        target_user.is_active = False
+        target_user.save()
+        return redirect('user_list')
+
+    return render(request, 'accounts/deactivate_user.html', {'target_user': target_user})
