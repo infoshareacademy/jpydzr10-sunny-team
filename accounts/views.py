5124389fc7a9fe7652007ee3ca65aa93f403da0c
Kubek0212 <kubek0212@gmail.com>
2026-05-16 23:10:35 +0200

    added user_list view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import User
+from permission import role_required
 
+@login_required
+@role_required('can_view_user_list')
+def user_list(request):
+    users = User.objects.all()
+    return render(request, 'accounts/user_list.html', {'users': users})
 # Create your views here.
+
