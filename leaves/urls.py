651fbbe5b642308867abfccb65c18a8833517a49
nowak-kamil <nkamil2002@gmail.com>
2026-05-21 16:38:22 +0200

    Done LeaveRequestUpdateView

--- leaves/urls.py
+++ leaves/urls.py
@@ -6,11 +6,12 @@ urlpatterns = [
     path('my-vacations/', views.my_vacations, name='my_vacations'),
     path('all-requests/', views.all_requests_list, name='all_requests_list'),
     path('new-request/', views.LeaveRequestView.as_view(), name='new_request'),
-    path('calculate-days/', views.calculate_days_api, name='calculate_days_api'),
     path('approve/<int:request_id>/', views.approve_request, name='approve_request'),
     path('reject/<int:request_id>/', views.reject_request, name='reject_request'),
     path('logs/', views.log_history, name='log_history'),
     path('team-balance/', views.team_leave_balance, name='team_leave_balance'),
     path('export-csv/', views.export_requests_csv, name='export_requests_csv'),
+    path('edit-request/<int:pk>/', views.LeaveRequestUpdateView.as_view(), name='leave_request_edit'),
+
 
 ]
