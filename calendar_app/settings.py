b9c8a33d0bf7c7d43d963c7062a7000a4308652a
Kubek0212 <33349024+Kubek0212@users.noreply.github.com>
2026-06-09 21:14:55 +0200

    Update settings.py

--- calendar_app/settings.py
+++ calendar_app/settings.py
@@ -88,8 +88,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'calendar_db',
         'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
+        'PASSWORD': '12345678',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
         #'OPTIONS': {
             #'unix_socket': '/tmp/mysql.sock',
