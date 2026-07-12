59c3fd64b77f504451fce318013ea5ddaeac9553
Kubek0212 <kubek0212@gmail.com>
2026-07-12 18:23:20 +0200

    added i18n template in templates

--- calendar_app/settings.py
+++ calendar_app/settings.py
@@ -77,6 +77,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'accounts.context_processors.active_role',
+                'django.template.context_processors.i18n',
             ],
         },
     },
