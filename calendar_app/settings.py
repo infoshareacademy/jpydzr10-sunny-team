0cd6e9982235b3dbafc8e183179858d433c7b62b
Patrykbartczak <patryk.bartczak99@gmail.com>
2026-07-15 17:35:15 +0200

    JPY101-156 SESSION_COOKIE_AGE + wygaszanie sesji po nieaktywnosci

--- calendar_app/settings.py
+++ calendar_app/settings.py
@@ -144,3 +144,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+## Session settings
+
+SESSION_COOKIE_AGE = 1800
+SESSION_SAVE_EVERY_REQUEST = True
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
