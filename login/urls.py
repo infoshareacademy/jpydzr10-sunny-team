d5cb21072a779eba0a3dcb96d4b070455570c085
Patrykbartczak <patryk.bartczak99@gmail.com>
2026-07-15 18:38:10 +0200

    JPY101-155 Formularz 2FA + walidacja w login_view

--- login/urls.py
+++ login/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+    path('', views.login_view, name='login'),
+    path('verify-2fa/', views.verify_2fa, name='verify_2fa')
 ]
--- login/views.py
+++ login/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth import authenticate, login, logout, get_user_model
 from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, redirect
-from logs.models import AuthLog
+from logs.models import AuthLog, EmailVerificationCode
 from logs.utils import get_client_ip, get_lockout_until, reset_failed_attempts, log_failed_attempt
+import random
 
 User = get_user_model()
 
@@ -64,13 +65,18 @@ def login_view(request):
                     ip_address=ip_address,
                     action='login_success',
                     severity='info',
-                    details='Poprawne logowanie.'
+                    details='Poprawne uwierzytelnienie hasłem. Rozpoczęto procedurę 2FA.'
                 )
+
+                # Udane logowanie hasłem - czyścimy próby dla IP
                 reset_failed_attempts(ip_address)
-                login(request, user)
-                return redirect('dashboard')
+                code = str(random.randint(100000, 999999))
+                EmailVerificationCode.objects.create(user=user, code=code)
+                request.session['2fa_user_id'] = user.id
+                print(f"KOD 2FA: {code}")  # tymczasowo zamiast maila
+                return redirect('verify_2fa')
         else:
-            # Formularz nie przeszedł walidacji (np. puste hasło lub nieistniejący user)
+            # Formularz nie przeszedł walidacji (np. nieistniejący użytkownik)
             new_lockout = log_failed_attempt(existing_user, username, ip_address)
             if new_lockout is not None:
                 return render(request, 'login.html', {
@@ -82,6 +88,7 @@ def login_view(request):
 
     return render(request, 'login.html', {'form': form})
 
+
 def logout_view(request):
     if request.user.is_authenticated:
         AuthLog.objects.create(
@@ -91,13 +98,38 @@ def logout_view(request):
             action='logout',
             severity='info',
             details='Poprawne wylogowanie.'
-
         )
     logout(request)
     return redirect('login')
 
 
 def home(request):
-    if request.user.is_authenticated:
-        pass
     return redirect('login')
+
+
+def verify_2fa(request):
+    user_id = request.session.get('2fa_user_id')
+    if not user_id:
+        return redirect('login')
+
+    if request.method == 'POST':
+        code_input = request.POST.get('code')
+        try:
+            user = User.objects.get(id=user_id)
+            verification = EmailVerificationCode.objects.filter(
+                user=user,
+                is_used=False
+            ).last()
+
+            if verification and verification.code == code_input:
+                verification.is_used = True
+                verification.save()
+                del request.session['2fa_user_id']
+                login(request, user)
+                return redirect('dashboard')
+            else:
+                return render(request, 'verify_2fa.html', {'error': 'Nieprawidłowy kod.'})
+        except User.DoesNotExist:
+            return redirect('login')
+
+    return render(request, 'verify_2fa.html')
