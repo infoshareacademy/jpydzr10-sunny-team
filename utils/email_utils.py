224ef9005a7ae22b87979f5696669d66eefe52ab
Grzegorz Tomza <firroth1991@gmail.com>
2026-07-17 16:09:15 +0200

    Create send welcome mail function

--- utils/email_utils.py
+++ utils/email_utils.py
@@ -3,9 +3,8 @@ from django.template.loader import render_to_string
 from django.conf import settings
 
 def send_approval_notification(employee_email, employee_name, request_details, site_url=None):
-    """
-    Wysyła mail do pracownika o zatwierdzeniu wniosku.
-    """
+    
+    # Wysyła mail do pracownika o zatwierdzeniu wniosku.
     if site_url is None:
         site_url = settings.SITE_URL
 
@@ -25,9 +24,8 @@ def send_approval_notification(employee_email, employee_name, request_details, s
     )
 
 def send_reject_notification(employee_email, employee_name, request_details, rejection_reason=None, site_url=None):
-    """
-    Wysyła mail do pracownika o odrzuceniu wniosku.
-    """
+    
+    # Wysyła mail do pracownika o odrzuceniu wniosku.
     if site_url is None:
         site_url = settings.SITE_URL
 
@@ -47,8 +45,9 @@ def send_reject_notification(employee_email, employee_name, request_details, rej
         fail_silently=False,
     )
 
-def send_new_request_notification(
-    manager_emails, employee_name, request_details, submission_date, site_url=None):
+def send_new_request_notification(manager_emails, employee_name, request_details, submission_date, site_url=None):
+
+    # Wysyła mail do Menager/HR o nowym wniosku.
     if site_url is None:
         site_url = settings.SITE_URL
     subject = f"Nowy wniosek urlopowy od {employee_name}"
@@ -59,13 +58,38 @@ def send_new_request_notification(
             "request_details": request_details,
             "submission_date": submission_date,
             "site_url": site_url,
-        },
+        }
     )
     send_mail(
         subject,
         "",
         settings.DEFAULT_FROM_EMAIL,
         manager_emails,
+        html_message=html_message,
+        fail_silently=False,
+    )
+
+def send_welcome_email(user_email, user_name, username, password, site_url=None):
+    
+    # Wysyła mail powitalny do nowego użytkownika z danymi logowania.
+    if site_url is None:
+        site_url = settings.SITE_URL
+
+    subject = f"Witaj w systemie SunnyTeam, {user_name}!"
+    html_message = render_to_string(
+        "emails/welcome_email.html",
+        {
+            "user_name": user_name,
+            "username": username,
+            "password": password,  # Uwaga: hasło jest widoczne w mailu tylko przy pierwszej rejestracji
+            "site_url": site_url,
+        }
+    )
+    send_mail(
+        subject,
+        "",
+        settings.DEFAULT_FROM_EMAIL,
+        [user_email],
         html_message=html_message,
         fail_silently=False,
     )
